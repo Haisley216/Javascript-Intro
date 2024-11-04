f3c037424412a7adc8fc928f68f5af3479f66d87
Kim Gahyeon <gimgahyeon@gimgahyeon-ui-MacBookAir.local>
2024-11-03 22:03:59 -0500

    Delete

--- 1/script.js
+++
@@ -1,9 +0,0 @@
-let string1 = "Merry Christmas";
-let string2 = "Happy Christmas";
-
-if (string1 == string2) {
-	alert("Equal");
-}
-else{
-	alert("Unequal");
-}
--- 3/script.js
+++
@@ -1,5 +0,0 @@
-function concatStr(string1, string2){
-	alert(string1 + string2);
-}
-
-concatStr("Bye", "Sunday");
