3cd3d0b55addd0fec5975343c51db363c2fd9a70
Kim Gahyeon <gimgahyeon@gimgahyeon-ui-MacBookAir.local>
2024-11-03 22:03:31 -0500

    delete

---
+++ 2/script.js
@@ -0,0 +1,12 @@
+let num1 = 23;
+let num2 = 230;
+
+if (num1 == num2) {
+	alert("num1 and num2 are equal");
+}
+else if (num1 > num2) {
+	alert("num1 is greater than num2");
+}
+else {
+	alert("num1 is less than num2");
+}
