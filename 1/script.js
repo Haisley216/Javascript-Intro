d128e69623e6179d96c6da9c4e20b9b0cb55bbd8
Kim Gahyeon <gimgahyeon@gimgahyeon-ui-MacBookAir.local>
2024-11-03 22:05:15 -0500

    add

---
+++ 1/script.js
@@ -0,0 +1,9 @@
+let string1 = "Merry Christmas";
+let string2 = "Happy Christmas";
+
+if (string1 == string2) {
+	alert("Equal");
+}
+else{
+	alert("Unequal");
+}
---
+++ 3/script.js
@@ -0,0 +1,5 @@
+function concatStr(string1, string2){
+	alert(string1 + string2);
+}
+
+concatStr("Bye", "Sunday");
